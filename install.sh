9e7ffc37c150c6767c684d9955699eac1434f7ce
Anthoni Nelli <anthoni.nelli@pm.me>
2024-02-12 20:57:17 +0100

    install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+# Update debian dependancies
+if command -v apt-get &> /dev/null; then
+  sudo apt-get update
+  sudo apt-get install -y bat
+fi
+
+conda install -c conda-forge starship -y
+
+starship preset nerd-font-symbols -o ~/.config/starship.toml
+
