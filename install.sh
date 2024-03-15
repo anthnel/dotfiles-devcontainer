c92af0088ebb9bf27dd84e4021f4c33ecb0c16a5
Anthoni Nelli <anthoni.nelli@pm.me>
2024-03-15 14:14:39 +0100

    install starship

--- install.sh
+++ install.sh
@@ -6,18 +6,19 @@ if command -v apt &> /dev/null; then
   sudo apt install -y zsh-autosuggestions zsh-syntax-highlighting httpie 
 fi
 
-# config git for line ending
+# config git endline
 git config --global core.autocrlf input
 
-# install starship and the preset
-conda install -c conda-forge starship -y
-starship preset nerd-font-symbols -o ~/.config/starship.toml
 
 # install k9s
 go install github.com/derailed/k9s@latest
 
 # install tilt
 curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
+
+# install starship
+curl -sS https://starship.rs/install.sh | sh
+
 
 # install fzf
 
@@ -27,6 +28,8 @@ fzfplugdir="$zshplug/fzf-zsh-plugin"
 [[ ! -d ${zshplug} ]] && mkdir -p ${zshplug}
 [[ ! -d ${fzfplugdir} ]] && git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${fzfplugdir}
 
+# config
+starship preset pure-preset -o ${HOME}/.config/starship.toml
  
 # link needed files
 ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
